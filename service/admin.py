e59e3ffe4fbba25c036c755c24f58c9f220b35b1
Bikee Prajapati <bikeeprajapati1@gmail.com>
2024-12-18 22:59:20 +0545

    Features Dynamic function added

--- service/admin.py
+++ service/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from service.models import Features
+
+class FeaturesAdmin(admin.ModelAdmin):
+    list_display = ('Features_Icon','Features_Title','Features_Description')
+    
+admin.site.register(Features,FeaturesAdmin)
 
 # Register your models here.
--- service/models.py
+++ service/models.py
@@ -2,4 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Features(models.Model):
-    pass
+    Features_Icon=models.CharField(max_length=50)
+    Features_Title=models.CharField(max_length=50)
+    Features_Description=models.TextField()
+    
